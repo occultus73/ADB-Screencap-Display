74001f93db20f8e85fd464eccc6faaf04216da5e
John O. A. Rayner-Hilles <johnhill9573@gmail.com>
2020-05-06 12:59:26 +0100

    Encapsulated the code to retrieve the image from ADB Screencap

---
+++ src/io/github/occultus73/Main.java
@@ -0,0 +1,45 @@
+package io.github.occultus73;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.io.InputStream;
+
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.WindowConstants;
+
+
+public class Main {
+	private static BufferedImage screenImage;
+	
+	private static JFrame frame;
+	private static JLabel label;
+	
+	public static void main(String[] args) {
+		
+		try {
+			screenImage = Screencap.getScreenImage();
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			if(screenImage == null) return;
+		}
+		
+		//Paint buffer image to window (likely exceed screen size).
+		label = new JLabel();
+		label.setIcon(new ImageIcon(screenImage));
+		
+		frame = new JFrame();
+		frame.setTitle("stained_image");
+		frame.setSize(screenImage.getWidth(), screenImage.getHeight());
+	    frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+		frame.getContentPane().add(label, BorderLayout.CENTER);
+	    frame.setLocationRelativeTo(null);
+	    frame.pack();
+	    frame.setVisible(true);
+	}
+
+}
